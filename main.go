a9d7918a7e1f2aad2d0206f391dc41c90d74899d
mpetavy <mpetavy>
2025-07-17 12:27:03 +0200

    common@v1.10.40 update

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"github.com/mpetavy/common"
+	"sync"
+	"sync/atomic"
 )
 
 //go:embed go.mod
@@ -12,7 +15,44 @@ func init() {
 	common.Init("", "", "", "", "", "", "", "", &resources, nil, nil, run, 0)
 }
 
+type Sperr struct {
+	Current atomic.Int64
+
+	mu sync.Mutex
+}
+
+var sp Sperr
+
+func (sp *Sperr) CanEnter() bool {
+	id := int64(common.GoRoutineId())
+
+	if sp.Current.CompareAndSwap(id, id) {
+		return false
+	}
+
+	sp.mu.Lock()
+
+	sp.Current.Store(id)
+
+	return true
+}
+
+func Error(msg error) bool {
+	if !sp.CanEnter() {
+		return msg != nil
+	}
+
+	defer sp.mu.Unlock()
+
+	Error(fmt.Errorf("DONT SHOW ERROR!!"))
+
+	fmt.Printf("%s\n", msg)
+
+	return msg != nil
+}
+
 func run() error {
+	Error(fmt.Errorf("test"))
 
 	return nil
 }
